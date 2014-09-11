0abaffe69e068b32562132151855a1e8a98e0eeb
marhazk <marhazk@yahoo.com>
2014-09-11 19:50:31 +0800

    Default

---
+++ NoToText/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace NoToText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIn = new System.Windows.Forms.TextBox();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnGen2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGen3 = new System.Windows.Forms.Button();
+            this.btnGen4 = new System.Windows.Forms.Button();
+            this.btnGen6 = new System.Windows.Forms.Button();
+            this.btnGen5 = new System.Windows.Forms.Button();
+            this.btnDHaz = new System.Windows.Forms.Button();
+            this.btnHaz = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIn
+            // 
+            this.txtIn.Location = new System.Drawing.Point(12, 37);
+            this.txtIn.Multiline = true;
+            this.txtIn.Name = "txtIn";
+            this.txtIn.Size = new System.Drawing.Size(318, 183);
+            this.txtIn.TabIndex = 0;
+            // 
+            // txtOut
+            // 
+            this.txtOut.Location = new System.Drawing.Point(345, 37);
+            this.txtOut.Multiline = true;
+            this.txtOut.Name = "txtOut";
+            this.txtOut.Size = new System.Drawing.Size(332, 183);
+            this.txtOut.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(218, 226);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(112, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "&Num to Text";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnGen2
+            // 
+            this.btnGen2.Location = new System.Drawing.Point(218, 255);
+            this.btnGen2.Name = "btnGen2";
+            this.btnGen2.Size = new System.Drawing.Size(112, 23);
+            this.btnGen2.TabIndex = 3;
+            this.btnGen2.Text = "&Text To Num";
+            this.btnGen2.UseVisualStyleBackColor = true;
+            this.btnGen2.Click += new System.EventHandler(this.btnGen2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Developed by MarHazK / Marhazli Kipli";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Coded Message";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(460, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Decoded Message";
+            // 
+            // btnGen3
+            // 
+            this.btnGen3.Location = new System.Drawing.Point(336, 226);
+            this.btnGen3.Name = "btnGen3";
+            this.btnGen3.Size = new System.Drawing.Size(112, 23);
+            this.btnGen3.TabIndex = 7;
+            this.btnGen3.Text = "&Hex to Text";
+            this.btnGen3.UseVisualStyleBackColor = true;
+            this.btnGen3.Click += new System.EventHandler(this.btnGen3_Click);
+            // 
+            // btnGen4
+            // 
+            this.btnGen4.Location = new System.Drawing.Point(336, 255);
+            this.btnGen4.Name = "btnGen4";
+            this.btnGen4.Size = new System.Drawing.Size(112, 23);
+            this.btnGen4.TabIndex = 8;
+            this.btnGen4.Text = "&T&ext to Hex";
+            this.btnGen4.UseVisualStyleBackColor = true;
+            this.btnGen4.Click += new System.EventHandler(this.btnGen4_Click);
+            // 
+            // btnGen6
+            // 
+            this.btnGen6.Location = new System.Drawing.Point(454, 255);
+            this.btnGen6.Name = "btnGen6";
+            this.btnGen6.Size = new System.Drawing.Size(112, 23);
+            this.btnGen6.TabIndex = 9;
+            this.btnGen6.Text = "&Text t&o MD5";
+            this.btnGen6.UseVisualStyleBackColor = true;
+            this.btnGen6.Click += new System.EventHandler(this.btnGen6_Click);
+            // 
+            // btnGen5
+            // 
+            this.btnGen5.Location = new System.Drawing.Point(454, 226);
+            this.btnGen5.Name = "btnGen5";
+            this.btnGen5.Size = new System.Drawing.Size(112, 23);
+            this.btnGen5.TabIndex = 10;
+            this.btnGen5.Text = "&MD5 to Text";
+            this.btnGen5.UseVisualStyleBackColor = true;
+            this.btnGen5.Click += new System.EventHandler(this.btnGen5_Click);
+            // 
+            // btnDHaz
+            // 
+            this.btnDHaz.Enabled = false;
+            this.btnDHaz.Location = new System.Drawing.Point(570, 255);
+            this.btnDHaz.Name = "btnDHaz";
+            this.btnDHaz.Size = new System.Drawing.Size(112, 23);
+            this.btnDHaz.TabIndex = 12;
+            this.btnDHaz.Text = "Text to H4&Z";
+            this.btnDHaz.UseVisualStyleBackColor = true;
+            this.btnDHaz.Click += new System.EventHandler(this.btnDHaz_Click);
+            // 
+            // btnHaz
+            // 
+            this.btnHaz.Enabled = false;
+            this.btnHaz.Location = new System.Drawing.Point(570, 226);
+            this.btnHaz.Name = "btnHaz";
+            this.btnHaz.Size = new System.Drawing.Size(112, 23);
+            this.btnHaz.TabIndex = 11;
+            this.btnHaz.Text = "&H4Z to Text";
+            this.btnHaz.UseVisualStyleBackColor = true;
+            this.btnHaz.Click += new System.EventHandler(this.btnHaz_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 284);
+            this.Controls.Add(this.btnDHaz);
+            this.Controls.Add(this.btnHaz);
+            this.Controls.Add(this.btnGen5);
+            this.Controls.Add(this.btnGen6);
+            this.Controls.Add(this.btnGen4);
+            this.Controls.Add(this.btnGen3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGen2);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.txtIn);
+            this.Name = "Form1";
+            this.Text = "Numthography by MarHazK";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIn;
+        private System.Windows.Forms.TextBox txtOut;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnGen2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGen3;
+        private System.Windows.Forms.Button btnGen4;
+        private System.Windows.Forms.Button btnGen6;
+        private System.Windows.Forms.Button btnGen5;
+        private System.Windows.Forms.Button btnDHaz;
+        private System.Windows.Forms.Button btnHaz;
+    }
+}
+
---
+++ NoToText/Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace NoToText
+{
+    public partial class Form1 : Form
+    {
+        public String text;
+        public String H4ZKey = "MarHazKHensem";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                text = txtIn.Text;
+                text = text.Replace("9999", "Z");
+                text = text.Replace("999", "Y");
+                text = text.Replace("99", "X");
+                text = text.Replace("9", "W");
+                text = text.Replace("888", "V");
+                text = text.Replace("88", "U");
+                text = text.Replace("8", "T");
+                text = text.Replace("7777", "S");
+                text = text.Replace("777", "R");
+                text = text.Replace("77", "Q");
+                text = text.Replace("7", "P");
+                text = text.Replace("666", "O");
+                text = text.Replace("66", "N");
+                text = text.Replace("6", "M");
+                text = text.Replace("555", "L");
+                text = text.Replace("55", "K");
+                text = text.Replace("5", "J");
+                text = text.Replace("444", "I");
+                text = text.Replace("44", "H");
+                text = text.Replace("4", "G");
+                text = text.Replace("333", "F");
+                text = text.Replace("33", "E");
+                text = text.Replace("3", "D");
+                text = text.Replace("222", "C");
+                text = text.Replace("22", "B");
+                text = text.Replace("2", "A");
+                text = text.Replace("111", ".");
+                text = text.Replace("1", ".");
+                text = text.Replace("000", ".");
+                text = text.Replace("00", ".");
+                text = text.Replace("0", " ");
+                txtOut.Text = text;
+            }
+            catch
+            {
+                MessageBox.Show("ERROR: Invalid Num Code");
+            }
+        }
+
+        private void btnGen2_Click(object sender, EventArgs e)
+        {
+            text = txtIn.Text.ToUpper();
+            text = text.Replace("A", "2");
+            text = text.Replace("B", "22");
+            text = text.Replace("C", "222");
+            text = text.Replace("D", "3");
+            text = text.Replace("E", "33");
+            text = text.Replace("F", "333");
+            text = text.Replace("G", "4");
+            text = text.Replace("H", "44");
+            text = text.Replace("I", "444");
+            text = text.Replace("J", "5");
+            text = text.Replace("K", "55");
+            text = text.Replace("L", "555");
+            text = text.Replace("M", "6");
+            text = text.Replace("N", "66");
+            text = text.Replace("O", "666");
+            text = text.Replace("P", "7");
+            text = text.Replace("Q", "77");
+            text = text.Replace("R", "777");
+            text = text.Replace("S", "7777");
+            text = text.Replace("T", "8");
+            text = text.Replace("U", "88");
+            text = text.Replace("V", "888");
+            text = text.Replace("W", "9");
+            text = text.Replace("X", "99");
+            text = text.Replace("Y", "999");
+            text = text.Replace("Z", "9999");
+            text = text.Replace(" ", "0");
+            text = text.Replace(".", "00");
+            txtOut.Text = text;
+        }
+
+        private void btnGen3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                text = txtIn.Text;
+                byte[] raw = new byte[text.Length / 2];
+                for (int i = 0; i < raw.Length; i++)
+                {
+                    raw[i] = Convert.ToByte(text.Substring(i * 2, 2), 16);
+                }
+
+                text = Encoding.ASCII.GetString(raw);
+                txtOut.Text = text;
+            }
+            catch
+            {
+                MessageBox.Show("ERROR: Invalid HEX Code");
+            }
+
+        }
+
+        private void btnGen4_Click(object sender, EventArgs e)
+        {
+            Encoding encoding = new System.Text.UTF8Encoding();
+            text = txtIn.Text;
+            txtOut.Clear();
+            int len = text.Length;
+            byte[] textB = encoding.GetBytes(text);
+            text = BitConverter.ToString(textB);
+            txtOut.Text = text.ToString().Replace("-", "");
+        }
+
+        private void btnGen6_Click(object sender, EventArgs e)
+        {
+
+            text = txtIn.Text;
+            System.Security.Cryptography.MD5CryptoServiceProvider x = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] bs = System.Text.Encoding.UTF8.GetBytes(text);
+            bs = x.ComputeHash(bs);
+            System.Text.StringBuilder s = new System.Text.StringBuilder();
+            foreach (byte b in bs)
+            {
+                s.Append(b.ToString("x2").ToLower());
+            }
+            txtOut.Text = s.ToString();
+        }
+
+        private void btnGen5_Click(object sender, EventArgs e)
+        {
+            text = txtIn.Text;
+            //string cipherText = text.Trim();
+            //string decryptedText = CryptorEngine.Decrypt(cipherText, true);
+            //txtOut.Text = decryptedText;
+            MessageBox.Show("Still under construction");
+            //test = (byte)("0x"+Convert.ToByte(text));
+        }
+        public static string EncryptString(string Message, string Passphrase)
+        {
+            byte[] Results;
+            System.Text.UTF8Encoding UTF8 = new System.Text.UTF8Encoding();
+
+            // Step 1. We hash the passphrase using MD5
+            // We use the MD5 hash generator as the result is a 128 bit byte array
+            // which is a valid length for the TripleDES encoder we use below
+
+            MD5CryptoServiceProvider HashProvider = new MD5CryptoServiceProvider();
+            byte[] TDESKey = HashProvider.ComputeHash(UTF8.GetBytes(Passphrase));
+
+            // Step 2. Create a new TripleDESCryptoServiceProvider object
+            TripleDESCryptoServiceProvider TDESAlgorithm = new TripleDESCryptoServiceProvider();
+
+            // Step 3. Setup the encoder
+            TDESAlgorithm.Key = TDESKey;
+            TDESAlgorithm.Mode = CipherMode.ECB;
+            TDESAlgorithm.Padding = PaddingMode.PKCS7;
+
+            // Step 4. Convert the input string to a byte[]
+            byte[] DataToEncrypt = UTF8.GetBytes(Message);
+
+            // Step 5. Attempt to encrypt the string
+            try
+            {
+                ICryptoTransform Encryptor = TDESAlgorithm.CreateEncryptor();
+                Results = Encryptor.TransformFinalBlock(DataToEncrypt, 0, DataToEncrypt.Length);
+            }
+            finally
+            {
+                // Clear the TripleDes and Hashprovider services of any sensitive information
+                TDESAlgorithm.Clear();
+                HashProvider.Clear();
+            }
+
+            // Step 6. Return the encrypted string as a base64 encoded string
+            return Convert.ToBase64String(Results);
+        }
+
+        public static string DecryptString(string Message, string Passphrase)
+        {
+            byte[] Results;
+            System.Text.UTF8Encoding UTF8 = new System.Text.UTF8Encoding();
+
+            // Step 1. We hash the passphrase using MD5
+            // We use the MD5 hash generator as the result is a 128 bit byte array
+            // which is a valid length for the TripleDES encoder we use below
+
+            MD5CryptoServiceProvider HashProvider = new MD5CryptoServiceProvider();
+            byte[] TDESKey = HashProvider.ComputeHash(UTF8.GetBytes(Passphrase));
+
+            // Step 2. Create a new TripleDESCryptoServiceProvider object
+            TripleDESCryptoServiceProvider TDESAlgorithm = new TripleDESCryptoServiceProvider();
+
+            // Step 3. Setup the decoder
+            TDESAlgorithm.Key = TDESKey;
+            TDESAlgorithm.Mode = CipherMode.ECB;
+            TDESAlgorithm.Padding = PaddingMode.PKCS7;
+
+            // Step 4. Convert the input string to a byte[]
+            byte[] DataToDecrypt = Convert.FromBase64String(Message);
+
+            // Step 5. Attempt to decrypt the string
+            try
+            {
+                ICryptoTransform Decryptor = TDESAlgorithm.CreateDecryptor();
+                int leng = DataToDecrypt.Length;
+                Results = Decryptor.TransformFinalBlock(DataToDecrypt, 0, leng);
+            }
+            finally
+            {
+                // Clear the TripleDes and Hashprovider services of any sensitive information
+                TDESAlgorithm.Clear();
+                HashProvider.Clear();
+            }
+
+            // Step 6. Return the decrypted string in UTF8 format
+            return UTF8.GetString(Results);
+        }
+
+        private void btnHaz_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                text = txtIn.Text.ToUpper();
+                byte[] raw = new byte[text.Length / 2];
+                //for (int i = 0; i < raw.Length; i++)
+                //{
+                //    raw[i] = Convert.ToByte(text.Substring(i * 2, 2), 16);
+                //}
+
+                //text = Encoding.ASCII.GetString(raw);
+
+                //text = text.ToString().Replace("-", "");
+                text = txtIn.Text.ToUpper();
+                string key = H4ZKey;
+                key = key.ToUpper();
+                text = text.Replace("9999" + key, "Z");
+                text = text.Replace("999" + key, "Y");
+                text = text.Replace("99" + key, "X");
+                text = text.Replace("9" + key, "W");
+                text = text.Replace("888" + key, "V");
+                text = text.Replace("88" + key, "U");
+                text = text.Replace("8" + key, "T");
+                text = text.Replace("7777" + key, "S");
+                text = text.Replace("777" + key, "R");
+                text = text.Replace("77" + key, "Q");
+                text = text.Replace("7" + key, "P");
+                text = text.Replace("666" + key, "O");
+                text = text.Replace("66" + key, "N");
+                text = text.Replace("6" + key, "M");
+                text = text.Replace("555" + key, "L");
+                text = text.Replace("55" + key, "K");
+                text = text.Replace("5" + key, "J");
+                text = text.Replace("444" + key, "I");
+                text = text.Replace("44" + key, "H");
+                text = text.Replace("4" + key, "G");
+                text = text.Replace("333" + key, "F");
+                text = text.Replace("33" + key, "E");
+                text = text.Replace("3" + key, "D");
+                text = text.Replace("222" + key, "C");
+                text = text.Replace("22" + key, "B");
+                text = text.Replace("2" + key, "A");
+                text = text.Replace("111" + key, ".");
+                text = text.Replace("1" + key, ".");
+                text = text.Replace("000" + key, ".");
+                text = text.Replace("00" + key, ".");
+                text = text.Replace("0" + key, " ");
+                text = text.ToString().Replace("-", "");
+                raw = new byte[text.Length / 2];
+                for (int i = 0; i < raw.Length; i++)
+                {
+                    raw[i] = Convert.ToByte(text.Substring(i * 2, 2), 16);
+                }
+
+                text = Encoding.ASCII.GetString(raw);
+                text = txtIn.Text.ToUpper();
+                txtOut.Clear();
+                txtOut.Text = text;
+            }
+            catch
+            {
+                MessageBox.Show("ERROR: Invalid HEX Code");
+            }
+        }
+
+        private void btnDHaz_Click(object sender, EventArgs e)
+        {
+
+            text = txtIn.Text.ToUpper();
+            Encoding encoding = new System.Text.UTF8Encoding();
+            txtOut.Clear();
+            int len = text.Length;
+            byte[] textB = encoding.GetBytes(text);
+            text = BitConverter.ToString(textB);
+            text = txtIn.Text.ToUpper();
+            text = text.ToString().Replace("-", "");
+            text = text.Replace("A", "2" + H4ZKey);
+            text = text.Replace("B", "22" + H4ZKey);
+            text = text.Replace("C", "222" + H4ZKey);
+            text = text.Replace("D", "3" + H4ZKey);
+            text = text.Replace("E", "33" + H4ZKey);
+            text = text.Replace("F", "333" + H4ZKey);
+            text = text.Replace("G", "4" + H4ZKey);
+            text = text.Replace("H", "44" + H4ZKey);
+            text = text.Replace("I", "444" + H4ZKey);
+            text = text.Replace("J", "5" + H4ZKey);
+            text = text.Replace("K", "55" + H4ZKey);
+            text = text.Replace("L", "555" + H4ZKey);
+            text = text.Replace("M", "6" + H4ZKey);
+            text = text.Replace("N", "66" + H4ZKey);
+            text = text.Replace("O", "666" + H4ZKey);
+            text = text.Replace("P", "7" + H4ZKey);
+            text = text.Replace("Q", "77" + H4ZKey);
+            text = text.Replace("R", "777" + H4ZKey);
+            text = text.Replace("S", "7777" + H4ZKey);
+            text = text.Replace("T", "8" + H4ZKey);
+            text = text.Replace("U", "88" + H4ZKey);
+            text = text.Replace("V", "888" + H4ZKey);
+            text = text.Replace("W", "9" + H4ZKey);
+            text = text.Replace("X", "99" + H4ZKey);
+            text = text.Replace("Y", "999" + H4ZKey);
+            text = text.Replace("Z", "9999" + H4ZKey);
+            text = text.Replace(" ", "0" + H4ZKey);
+            //text = text.Replace(".", "00" + H4ZKey);
+            //encoding = new System.Text.UTF8Encoding();
+            //txtOut.Clear();
+            //len = text.Length;
+            //textB = encoding.GetBytes(text);
+            //text = BitConverter.ToString(textB);
+            //text = text.ToString().Replace("-", "");
+            text = text.ToLower();
+            txtOut.Text = text;
+        }
+    }
+}
